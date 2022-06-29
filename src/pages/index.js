e781b88450143f316f0151f83f53413b70b509f4
Saurab Gami <saurabgami977@gmail.com>
2022-06-29 20:31:13 +0545

    unneeded components removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,9 @@ import { Grid } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-import WalletInfo from "../Components/WalletInfo/WalletInfo";
-
 import { BASE_API } from "../Constant";
 
 const Homepage = () => {
-	const [wallet, setWallet] = useState();
-
-	useEffect(() => {
-		fetchWalet();
-	}, []);
-
-	const fetchWalet = () => {
-		axios.get(BASE_API + `/api/wallet-info`).then((res) => setWallet(res.data));
-	};
-
 	return <Grid container spacing={2} padding={3}></Grid>;
 };
 
